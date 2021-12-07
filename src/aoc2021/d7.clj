a6bc6c599701335de170ef390fac68eb061ebe2c
William Ganrot <williamganrot@hotmail.com>
2021-12-07 09:04:01 +0100

    day 7

---
+++ src/aoc2021/d7.clj
@@ -0,0 +1,30 @@
+(ns aoc2021.d7
+  (:require [clojure.string :as str]
+            [clojure.math.numeric-tower :as math]))
+
+(def test-input (as-> "16,1,2,0,4,2,7,1,2,14" $
+                  (str/split $ #",")
+                  (map #(Integer/parseInt %) $)))
+
+(def input (as-> "resources/d7.txt" $
+             (slurp $)
+             (str/split $ #",")
+             (map #(Integer/parseInt %) $)))
+
+(defn total-distances-to [pos crabs step-inc]
+  (->> crabs
+       (map #(math/abs (- pos %)))
+       (map #(+ % (->> (range %)
+                       (map (fn [v] (* v step-inc)))
+                       (reduce +))))
+       (reduce +)))
+
+(defn solve [input step-inc]
+  (reduce (fn [min pos]
+            (let [total-distance (total-distances-to pos input step-inc)]
+              (if (< total-distance min)
+                total-distance
+                min)))
+          ##Inf (range (reduce min input) (inc (reduce max input)))))
+
+(solve test-input 1)
